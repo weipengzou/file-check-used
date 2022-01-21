b24a5356765afe5974159803679feecbb910aa00
weipengzou <944740645@qq.com>
2022-01-21 17:28:58 +0800

    feat: origin

---
+++ readme.md
@@ -0,0 +1,11 @@
+### Installation(安装)
+
+```
+npm install file-check-used -g
+```
+
+### Usage(使用)
+
+```
+fcu
+```
---
+++ src/searchAssetsFile.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import fs from 'fs';
+export const getTargetFileArr = (targetFilePath: any) => {
+  let resultArr: {
+    name: string;
+    file: string;
+  }[] = [];
+
+  // 遍历文件夹
+  const readDir = (filePath: any) => {
+    // 遍历文件目录
+    fs.readdirSync(filePath).forEach((fileName) => {
+      if (/node_modules|dist/.test(filePath)) return;
+
+      const file = path.join(filePath, fileName);
+      const info = fs.statSync(file);
+      // 递归目录
+      if (info.isDirectory()) readDir(file);
+      // 文件
+      else {
+        resultArr.push({
+          file: file,
+          name: fileName,
+        });
+      }
+    });
+  };
+  readDir(targetFilePath);
+  console.log('文件总数：', resultArr.length);
+  return resultArr;
+};
